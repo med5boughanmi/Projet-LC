d237139876e1f2749f0836d1b74e8723fa1932bd
med5boughanmi <boughanmimohamedlazher@gmail.com>
2016-05-09 15:19:27 +0100

    refonte des fichiers

--- menu/menu.php
+++ menu/menu.php
@@ -17,13 +17,13 @@
 					<span class=" menu-text glyphicon glyphicon-sort"> Stock </span> <b class="arrow fa fa-angle-down"></b> 
 						</a> <b class="arrow"></b>
 						<ul class="submenu">
-							<li class=""><a href="visitorslist.php"> <i class="glyphicon glyphicon-log-in"></i> Entrées </a>
+							<li class=""><a href="#"> <i class="glyphicon glyphicon-log-in"></i> Entrées </a>
 							 <b  class="arrow"></b> </li>
-							<li class=""> <a href="newvisitor.php"><i class="glyphicon glyphicon-log-out"></i> Sorties </a>
+							<li class=""> <a href="#"><i class="glyphicon glyphicon-log-out"></i> Sorties </a>
 							 <b  class="arrow"></b></li>
 						</ul>
 					</li>
-					 <li class=""> <a href="fishingseason.php" > 
+					 <li class=""> <a href="#" > 
 							<span class="menu-text glyphicon glyphicon-list-alt"> Articles</span> <b>  </b>
 						</a> <b class="arrow"></b>
 					
@@ -33,13 +33,13 @@
 						 <span class="menu-text glyphicon glyphicon-shopping-cart"> Achats </span> <b class="arrow fa fa-angle-down"></b>
 						</a> <b class="arrow"></b>
 						<ul class="submenu">
-							<li class=""><a href="woodboat.php"> Commande </a>
+							<li class=""><a href="#"> Commande </a>
 							 <b  class="arrow"></b> </li>
-							<li class=""> <a href="alluboat.php"> Depenses </a>
+							<li class=""> <a href="#"> Depenses </a>
 							 <b  class="arrow"></b></li>
-							 <li class=""> <a href="fiberboat.php"> Factures reçues </a>
+							 <li class=""> <a href="#"> Factures reçues </a>
 							 <b  class="arrow"></b></li>
-							  <li class=""> <a href="fiberboat.php"> Historique</a>
+							  <li class=""> <a href="#"> Historique</a>
 							 <b  class="arrow"></b></li>
 						</ul>
 					</li>
@@ -49,15 +49,15 @@
 							<span class="menu-text glyphicon glyphicon-euro"> Ventes </span> <b class="arrow fa fa-angle-down"></b>
 						</a> <b class="arrow"></b>
 						<ul class="submenu">
-							<li class=""><a href="yamaha.php">  Les imports</span></a>
+							<li class=""><a href="#">  Les imports</span></a>
 							 <b  class="arrow"></b> </li>
-							<li class=""> <a href="tohatsu.php"> Les exports </i> </a>
+							<li class=""> <a href="#"> Les exports </i> </a>
 							 <b  class="arrow"></b></li>
 							 
 						</ul>
 					</li>
 					
-					<li class=""> <a href="Workshop.php" > 
+					<li class=""> <a href="#" > 
 							<span class="menu-text glyphicon glyphicon-briefcase"> Comptabilité</span> <b>  </b>
 						</a> <b class="arrow"></b>
 					
